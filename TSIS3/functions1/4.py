d111a3b9a120c3a17f63850f4f8ca687aa4f9419
Nurzhan Kurmangazy <n_kurmangazy@kbtu.kz>
2023-02-05 09:45:41 +0300

    functions1

--- TSIS3/functions1/4.py
+++ TSIS3/functions1/4.py
@@ -1,15 +1,18 @@
-def filter_prime(it:list):
-    prime = []
-    for i in range(len(it)):
-        for j in range(2, it[i]):
+def filter_prime(c):
+    ok = True
+    for i in range(2, c):
+        if c % i == 0:
+            ok = False
+            break
+    return ok
 
-            if it[i]%j==0:
-                break
-            else:
-                prime.append(it[i])
-        else:
-            continue
-    print(set(prime))
-it = [1,2,3,4,5,6,7,8,9,10,11,12,13, 15, 16, 17 ,18, 20, 23, 24]
-filter_prime(it)
+
+if __name__ == "__main__":
+    a =  int(input())
+    l = []
+    for i in range(a):
+        c = int(input())
+        if filter_prime(c):
+            l.append(c)
+    print(l)
 
--- TSIS3/functions1/9.py
+++ TSIS3/functions1/9.py
@@ -1,6 +1,7 @@
 def volume(rad:float):
     vol = pow(rad, 3)
-    vol2 = (vol * 3 * 3,14)/3
-    print(vol2)
+    vol2 = (vol * 4)/3
+    vol3 = vol2 * 3.14
+    print(vol3)
 rad = float(input("Enter the radius: "))
 volume(rad)
